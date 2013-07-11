db6955277300e853950bdf4ea39fd98f3c9e3c31
Mrinal Iyer <mrinal@umich.edu>
2013-07-11 02:13:43 +0000

    trying to read in index.html

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+function readIndex(){
+    var index = fs.readFileSync('index.html','utf-8');
+    return index;
+}
+
 app.get('/', function(request, response) {
-  response.send('Added index.html');
+
+    var hello = readIndex();
+    response.send('Trying to read in index.html');
 });
 
 var port = process.env.PORT || 5000;
